32195a25d6f4d62e07222ad9930c5f61d7d4c370
mrabadi <mrabadi@justanotherbrain.com>
2018-10-25 21:27:37 -0400

    fix demo2 tone hack

--- demo2.py
+++ demo2.py
@@ -150,7 +150,7 @@ while trial_number < num_conditions and not quit:
             else:
                 response = 'invalid'
         event.clearEvents()
-    print('\q')
+    print('\a')
     fixation.draw()
     win.flip()
     trial_data['response'] = response if response is not None else 'TIMEOUT'
